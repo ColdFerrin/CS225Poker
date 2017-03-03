5885cff81ac21c51765869fc4a78670002ac2015
Ferrin Benjamin Katz <ferrinkatz@ferrinkatz.com>
2017-03-02 22:24:25 -0700

    Ferrin: added code to Deck constructor to generate deck and added get card and burn card methods to Deck

--- Poker Game/Poker Game/Deck.cpp
+++ Poker Game/Poker Game/Deck.cpp
@@ -1,12 +1,72 @@
 #include "Deck.h"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
 
-
+using namespace std;
 
 Deck::Deck()
 {
+    bool generated[4][13] = { 0 };
+    for (int traverse = 0; traverse < 52; traverse++)
+    {
+        bool isCardValid = 0;
+        int suit;
+        int rank;
+        while (!isCardValid)
+        {
+            srand(time(NULL));
+
+            rank = rand() % 13;
+            suit = rand() % 4;
+
+            if (generated[suit][rank] == 0)
+            {
+                generated[suit][rank] = 1;
+                deck[traverse].rank = rank + 2;
+                if (suit == 0)
+                {
+                    deck[traverse].suit = 's';
+                }
+                else if (suit == 1)
+                {
+                    deck[traverse].suit = 'h';
+                }
+                else if (suit == 2)
+                {
+                    deck[traverse].suit = 'c';
+                }
+                else if (suit == 3)
+                {
+                    deck[traverse].suit = 'd';
+                }
+                isCardValid = true;
+            }
+        }
+    }
+
+    cout << "The deck has been shuffled" << endl;
 }
 
 
 Deck::~Deck()
 {
+
+}
+
+CARD Deck::getCard()
+{
+    CARD temp;
+
+    temp.rank = deck[deckPosition].rank;
+    temp.suit = deck[deckPosition].suit;
+    
+    deckPosition++;
+
+    return temp;
+}
+
+void Deck::burnCard()
+{
+    deckPosition++;
 }
--- Poker Game/Poker Game/Deck.h
+++ Poker Game/Poker Game/Deck.h
@@ -1,13 +1,20 @@
 #pragma once
 
 #ifndef DECK_H
-#define DECH_H
+#define DECK_H
+
+#include "Card.h"
 
 class Deck
 {
+private:
+    int deckPosition;
+    CARD deck[52];
 public:
     Deck();
     ~Deck();
+    CARD getCard();
+    void burnCard();
 };
 
 #endif // !DECK_H
