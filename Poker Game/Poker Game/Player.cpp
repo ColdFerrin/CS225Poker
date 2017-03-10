96cc3051045cf732aecff757205e71f92d77413e
Ferrin Benjamin Katz <ferrinkatz@ferrinkatz.com>
2017-03-09 21:45:15 -0700

    Ferrin: Added methods to player base class to clear hand put cards in hand and pure virtual method to make decisions.

--- Poker Game/Poker Game/Player.cpp
+++ Poker Game/Poker Game/Player.cpp
@@ -1,11 +1,42 @@
 #include "Player.h"
 
+void Player::collectCard(CARD newCard)
+{
+	if (cardsRecived < 2)
+	{
+		hand[cardsRecived].suit = newCard.suit;
+		hand[cardsRecived].rank = newCard.rank;
+	}
+}
 
+int Player::cardsInHand()
+{
+	switch (cardsRecived)
+	{
+	case 0:
+		return 0;
+		break;
+	case 1:
+		return 1;
+		break;
+	case 2:
+		return 2;
+		break;
+	default:
+		return 0;
+		break;
+	}
+}
+
+void Player::clearHand()
+{
+	cardsRecived = 0;
+}
 
 Player::Player()
 {
+	cardsRecived = 0;
 }
-
 
 Player::~Player()
 {
--- Poker Game/Poker Game/Player.h
+++ Poker Game/Poker Game/Player.h
@@ -1,8 +1,22 @@
 #pragma once
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Card.h"
+
 class Player
 {
+private:
+	CARD hand[2];
+	int cardsRecived;
 public:
-    Player();
-    ~Player();
+	virtual int makeDecision() = 0;
+	void collectCard(CARD);
+	int cardsInHand();
+	void clearHand();
+	Player();
+	~Player();
 };
 
+#endif // !PLAYER_H
